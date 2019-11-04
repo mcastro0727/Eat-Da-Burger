9ddc6755fbaf6640162457aa8d9b84d5cd2f9959
Melissa Castro <melissa.anne.castro@gmail.com>
2019-11-03 21:07:40 -0500

    added directories and inital js and backend code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Baby Shark');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Small Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tiny Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fake Meat Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Cluck Cluck Burger');
