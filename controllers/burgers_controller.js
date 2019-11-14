53bdccd6f77c4b3d60d0129d3c4de17f9b0b7620
Melissa Castro <melissa.anne.castro@gmail.com>
2019-11-13 23:54:38 -0500

    revised '/' route again

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,11 +6,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  res.redirect("burgers")
-});
-
-router.get("/burgers", function(req,res){
+router.get("/", function(req,res){
   burger.all(function(data) {
     var hbsObject = {
       burgers: data
@@ -26,7 +22,7 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.burger_name
   ], function(data) {
-    res.redirect("/burgers");
+    res.redirect("/");
   });
 });
 
@@ -38,7 +34,7 @@ router.put("/api/burgers/:id", function(req, res) {
   burger.update({
 		"devoured": req.body.devoured
 	}, condition, function(data){
-		res.redirect("/burgers")
+		res.redirect("/")
 	});
 });
 
